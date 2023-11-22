4b7c0776781039c4d933cf9b229deeafb058910d
clarayuan <c_w_yuan@hotmail.com>
2023-11-22 10:09:59 -0500

    starting the Dijkstras

---
+++ src/main/java/org/example/hackerrank/Graph.java
@@ -0,0 +1,45 @@
+package org.example.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ *
+ */
+public class Graph {
+
+    /*
+     * <a href="https://www.hackerrank.com/challenges/bfsshortreach/problem">BFS problem</a>
+     * Complete the 'bfs' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n (number of integers we are working, 1, 2, 3...)
+     *  2. INTEGER m
+     *  3. 2D_INTEGER_ARRAY edges
+     *  4. INTEGER s
+     */
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/bfsshortreach/problem">BFS problem</a>
+     * Complete the 'bfs' function below.
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n (number of integers we are working, 1, 2, 3..., n)
+     *  2. INTEGER m (number of edges, equals the size of edges list)
+     *  3. 2D_INTEGER_ARRAY edges
+     *  4. INTEGER s (the starting node)
+     *  the goal is to get the shortest distance.
+     *  each edge is worth $6
+     */
+    public static List<Integer> bfs(int n, int m, List<List<Integer>> edges, int s) {
+        // Write your code here
+        List<Integer> retval = new ArrayList<>();
+
+        // will be attempting it in the neetcode DijkstrasAlgo
+
+        return retval;
+    }
+
+}
---
+++ src/main/java/org/example/neetcode/DijkstrasAlgo.java
@@ -0,0 +1,4 @@
+package org.example.neetcode;
+
+public class DijkstrasAlgo {
+}
